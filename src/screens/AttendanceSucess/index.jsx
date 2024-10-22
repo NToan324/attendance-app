d9f2cb2acef0f3455fab03d4927bc0a39c3ee064
NToan324 <nhattoan664t@gmail.com>
2024-10-22 10:42:21 +0700

    up:code

--- src/screens/AttendanceSucess/index.jsx
+++ src/screens/AttendanceSucess/index.jsx
@@ -6,7 +6,7 @@ export default function index() {
   return (
     <div className="w-full h-full min-h-screen flex flex-col justify-center items-center bg-white">
       <img src={Congratulations} alt="Loading" className="w-60 h-60" />
-      <h1 className="text-center text-3xl font-bold">Điểm danh thành công!</h1>
+      <h1 className="text-center text-2xl font-bold">Điểm danh thành công!</h1>
     </div>
   );
 }
