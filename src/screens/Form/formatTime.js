4fcf081c27ae15b4cbf559c12ee1200e42c93fab
NToan324 <nhattoan664t@gmail.com>
2024-11-04 19:01:42 +0700

    change format

--- src/screens/Form/formatTime.js
+++ src/screens/Form/formatTime.js
@@ -7,6 +7,6 @@ const formatDate = () => {
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
 
-  return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 };
 export default formatDate;
